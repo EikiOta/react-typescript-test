57a00fce4e22112fecb3cf170e94da0dd19f43f1
太田瑛棋 <nezirekokkai0929@icloud.com>
2022-07-13 23:50:57 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+class App extends React.Component {
+ 
+  constructor(props:string) {
+    super(props)
+    this.state = {name:"SK99"}
+  }
+  render() {
+    return (
+      <div>
+    <h1>hoge</h1>
+      </div>
+      );
+  }
+
+}
+
+// function App() {
+//   return (
+//   <div>
+
+//   </div>
+//   );
+// }
+
+export default App;
